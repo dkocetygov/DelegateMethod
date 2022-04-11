b5326d7aa055fff7bd6259ba355b95c76715c307
wsr11 <wsr11@college.edu>
2022-04-11 12:58:47 +0600

    Добавьте файлы проекта.

---
+++ DelegateMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateMethod
+{
+    public delegate string HelloDelegate(string name);
+
+    class SomeClass
+    {
+        public string SayHello(string s)
+        {
+            return $"Привет, {s}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            HelloDelegate hD = new HelloDelegate(someClass.SayHello);
+            Console.WriteLine(hD.Invoke("Вася"));
+            Console.ReadKey();
+            Console.WriteLine(hD("Вася"));
+        }
+    }
+}
